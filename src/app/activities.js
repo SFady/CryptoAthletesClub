8723812da3c35ca578e5611be71910bf990445bd
BMenez <bmenez@laposte.net>
2025-08-07 12:31:32 +0200

    data

--- src/app/activities.js
+++ src/app/activities.js
@@ -1,4 +1,5 @@
 export const activities = [
+{ id: 257, date: "07/08/2025", utilisateur: "Nico_Robin", activite: "Running", defit: 100.05, participation: "50%", defitnet: 50.03, km: 13.20 },
 { id: 256, date: "07/08/2025", utilisateur: "Usopp", activite: "Running", defit: 37.13, participation: "100%", defitnet: 37.13, km: 10.30 },
 { id: 255, date: "06/08/2025", utilisateur: "Usopp", activite: "Running", defit: 10.42, participation: "100%", defitnet: 10.42, km: 4.70 },
 { id: 254, date: "05/08/2025", utilisateur: "Nico_Robin", activite: "Marche", defit: 9.47, participation: "50%", defitnet: 4.74, km: 3.10 },
