bc6f56f1e710339c86d920aaea41c183fe0439fd
BMenez <bmenez@laposte.net>
2025-12-07 11:02:15 +0100

    data

--- src/app/activities.js
+++ src/app/activities.js
@@ -1,4 +1,5 @@
 export const activities = [
+{ id: 390, date: "07/12/2025", utilisateur: "Usopp", activite: "Running", defit: 243.3, participation: "100%", defitnet: 243.30, km: 15.80 },
 { id: 389, date: "05/12/2025", utilisateur: "Usopp", activite: "Marche", defit: 8.28, participation: "100%", defitnet: 8.28, km: 2.50 },
 { id: 388, date: "05/12/2025", utilisateur: "Usopp", activite: "Running", defit: 163.36, participation: "100%", defitnet: 163.36, km: 12.20 },
 { id: 387, date: "05/12/2025", utilisateur: "Usopp", activite: "Marche", defit: 6.31, participation: "100%", defitnet: 6.31, km: 2.20 },
