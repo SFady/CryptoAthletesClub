4e2445386a1b8c5a93a483188cd853c5742adbcb
BMenez <bmenez@laposte.net>
2026-03-20 09:17:08 +0100

    info bulle

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -207,13 +207,21 @@ export default function Home() {
                   <span
                     onClick={() => setOpen(!open)}
                     className="ml-1 relative inline-flex items-center justify-center 
-                     w-4 h-4 text-xs border border-white/50 rounded-full cursor-pointer align-middle"
+                 w-4 h-4 text-xs border border-white/50 rounded-full 
+                 cursor-pointer align-middle"
                   >
                     ?
+
                     {open && (
-                      <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 
-                             bg-black text-white text-xs px-2 py-1 rounded whitespace-nowrap">
-                        Defits actuellement bloqués et non récupérables.
+                      <span
+                        className="absolute bottom-full mb-2 
+                     left-0 sm:left-1/2 sm:-translate-x-1/2
+                     max-w-[200px] px-2 py-1 
+                     bg-black text-white text-xs rounded 
+                     whitespace-pre-line text-left sm:text-center
+                     shadow-lg z-10"
+                      >
+                        Defits actuellement bloqués{`\n`}et non récupérables.
                       </span>
                     )}
                   </span>
