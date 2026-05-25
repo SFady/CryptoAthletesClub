742cf2044d77a64dda9e2c32c4e9cefef7c6908c
BMenez <bmenez@laposte.net>
2026-05-25 10:16:55 +0200

    Bonus

--- src/app/ClientGate.js
+++ src/app/ClientGate.js
@@ -37,16 +37,16 @@ const MESSAGES = [
     {
         key: "infoMessage_v1",
         title: "Félicitations",
-        startDate: new Date("2026-05-18T00:00:00"),
-        deadline: new Date("2026-05-24T00:00:00"),
+        startDate: new Date("2026-05-25T00:00:00"),
+        deadline: new Date("2026-05-31T00:00:00"),
         confetti: true,
         content: (
             <div className="flex items-start gap-4">
                 <GiTrophy className="w-16 h-16 shrink-0 mt-1 text-[#D6C48A]" />
                 <ul className="text-left list-disc pl-4 space-y-2 text-white/80 text-sm sm:text-base">
-                    <li>Grand vainqueur de la plus longue distance de running hebdomadaire : Usopp</li>
-                    <li>Distance de 61.52 km.</li>
-                    <li>Prix exceptionnel de 1.88 $ !!!</li>
+                    <li>Grand vainqueur de la plus longue distance de running hebdomadaire : Nico Robin</li>
+                    <li>Distance de 30.50 km.</li>
+                    <li>Prix exceptionnel de 0.94 $ !!!</li>
                 </ul>
             </div>
         ),
